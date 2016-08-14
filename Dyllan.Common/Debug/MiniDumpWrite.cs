c3a333aeb6cdbc5f9ec8c1e0a2611d5ffd507f85
DyllanLuo <ecjtu_luowei@outlook.com>
2016-08-14 12:56:47 +0800

    Create mini dump method

---
+++ Dyllan.Common/Debug/MiniDumpWrite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Dyllan.Common.Debug
+{
+    internal static class MiniDumpWriter
+    {
+        [DllImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+        internal static extern bool MiniDumpWriteDump(IntPtr hProcess, uint processId, IntPtr hFile, uint dumpType, IntPtr expParam,
+            IntPtr userStreamParam, IntPtr callbackParam);
+    }
+}
---
+++ Dyllan.Common/Debug/MiniDumpWriterHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Dyllan.Common.Debug
+{
+    public static class MiniDumpWriterHelper
+    {
+        const string DateTimeFormat = "yyyyMMdd-HHmmss.fff";
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static bool Write(string fileNamePrefix, string directoryName, MiniDumpType options = MiniDumpType.MiniDumpWithFullMemory
+            | MiniDumpType.MiniDumpWithFullMemoryInfo | MiniDumpType.MiniDumpWithDataSegs | MiniDumpType.MiniDumpWithUnloadedModules
+            | MiniDumpType.MiniDumpWithThreadInfo | MiniDumpType.MiniDumpWithHandleData)
+        {
+            bool result = false;
+            if (!Directory.Exists(directoryName))
+                Directory.CreateDirectory(directoryName);
+
+            string fileName = Path.Combine(directoryName, string.Format("{0}-{1}.dmp", fileNamePrefix, DateTime.Now.ToString(DateTimeFormat)));
+
+            try
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.Write))
+                {
+                    Process currentProcess = Process.GetCurrentProcess();
+                    IntPtr currentProcessHandle = currentProcess.Handle;
+                    uint currentProcessId = (uint)currentProcess.Id;
+
+                    result = MiniDumpWriter.MiniDumpWriteDump(currentProcessHandle, currentProcessId, fs.SafeFileHandle.DangerousGetHandle(),
+                        (uint)options, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+                }
+            }
+            catch (Exception)
+            {
+                // WriteLog
+            }
+
+            return result;
+        }
+    }
+}
